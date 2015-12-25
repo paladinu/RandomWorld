0d29f35acb7568ef93625f7b55184bcf12e2e5bc
Uriah Blatherwick <uriahb@gmail.com>
2015-12-25 15:23:54 -0600

    Minified files

--- dist/all.js
+++ dist/all.js
@@ -1,1 +1,17 @@
 function test(){console.log('hey');}
+function GenerateForm(data){
+	var self = this;
+	
+	self.Generate = function(){
+		//TODO: call generate function on Server passing settings to get result
+		var result = new PersonResult({
+			Sex: 'Male'
+		});	
+		ko.applyBindings(result, document.getElementsByClassName('results-display'));
+	};
+}
+function PersonResult(data){
+	var self = this;
+	
+	self.Sex = ko.observable(data.Sex);
+}
